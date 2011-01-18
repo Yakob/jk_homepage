87158afba31d03bbbf9da8efe69dc4b19aa32be7
Yakob <yakob88@o2.pl>
2011-01-18 18:43:50 +0100

    Charset fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  before_filter :set_charset
-  def set_charset
-    @headers["Content-Type"] = "text/html; charset=UTF-8" 
-  end
 end
